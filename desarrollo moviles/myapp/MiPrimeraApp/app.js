d85226424b83d7ad4e2ecb68de226f7508a79ad5
zpjonathan <adairzohan@gmail.com>
2025-08-24 20:01:57 -0600

    repo actualizado

---
+++ desarrollo moviles/myapp/MiPrimeraApp/app.js
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+import {Dimensions, FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+const{width,height}= Dimensions.get('window');
+console.log(width,height);
+export default function App(){
+
+
+
+
+    
+}
